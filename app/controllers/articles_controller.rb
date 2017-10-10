0a20ea91c74b454e3bd45886641bfaa55e7d1b04
Chase <chase.gilliam@gmail.com>
2017-10-09 21:49:32 -0400

    refactoring articles controller a bit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+# Controller for Articles and related search functionality
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show]
   before_action :validate_search, only: [:results]
@@ -8,41 +9,42 @@ class ArticlesController < ApplicationController
   end
 
   def results
-    @publications = articles_search_params[:publications]
-    @title_search = articles_search_params[:title]
-    @search_offset = articles_search_params[:search_offset].to_i || 0
+    @publications = search_params[:publications]
+    @title_search = search_params[:title]
+    @search_offset = search_params[:search_offset].to_i || 0
     @articles =
       Articles::Finder.find(@publications, @title_search, @search_offset)
   end
 
   def show
-    unless @article
-      flash[:error] = 'The requested article does not exist'
-      redirect_to root_url
-    end
+    flash_and_redirect('The requested article does not exist') unless @article
   end
 
   def search
-    @articles_search_form = ArticlesSearchForm.new
+    @articles_search_form ||= ArticlesSearchForm.new
   end
 
   private
+
+  def flash_and_redirect(message, flash_type = :error, redirect_path = root_url)
+    flash[flash_type] = message
+    redirect_to(redirect_path)
+  end
+
   def validate_search
-    if articles_search_params['title'].blank?
-      flash[:error] = 'You must include a title to search for articles'
-      redirect_to root_url
-    end
+    error_message = 'You must include a title to search for articles'
+    flash_and_redirect(error_message) if search_params['title'].blank?
   end
 
   def find_article
-    @article = Article.where(uuid: article_show_params['id']).first
+    @article = Article.find_by(uuid: show_params['id'])
   end
 
-  def article_show_params
+  def show_params
     params.permit('id')
   end
 
-  def articles_search_params
+  def search_params
     params.require(:articles_search_form)
           .permit(:publications, :title, :search_offset)
   end
