848165ea8b7a7c9511fb4454107b94298959fe17
Chase <chase.gilliam@gmail.com>
2017-10-15 12:54:15 -0400

    grid based nav and fade overlay

--- app/assets/javascripts/navToggle.js
+++ app/assets/javascripts/navToggle.js
@@ -2,10 +2,12 @@ document.addEventListener('turbolinks:load', function() {
   
   const mobileMenu = document.querySelector('.mobile-menu');
   const toggle = document.querySelector('.nav-toggle');
+  const screen = document.querySelector('.screen');
   toggle.onclick = function(e){
     if (mobileMenu.classList && !mobileMenu.classList.contains('active')) {
       mobileMenu.classList.remove('slide-back');
       mobileMenu.classList.add('active');
+      screen.classList.add('overlay');
     }
   }
   
@@ -16,6 +18,7 @@ document.addEventListener('turbolinks:load', function() {
     if (!isClickInside && !clickedOnToggle && menuActive) {
       mobileMenu.classList.remove('active');
       mobileMenu.classList.add('slide-back');
+      screen.classList.remove('overlay');
     }
   });
 })
